b0f66f11ca6f1696a833370c7aa62dab093c8d93
vishnu-67 <Vishnu@dartexon.com>
2022-08-29 17:14:17 +0530

    init codes

---
+++ clients/Admin/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  clientId: '$APPCLIENTID',
+  issuer: '$AUTHSERVERURL',
+  customDomain: '$AUTHCUSTOMDOMAIN',
+  apiUrl: 'https://api.$CUSTOM_DOMAIN',
+  domain: '$CUSTOM_DOMAIN',
+};
---
+++ clients/Application/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.eks-ref-arch.com',
+  domain: 'eks-ref-arch.com',
+};
